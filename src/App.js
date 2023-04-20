e464e8c415833b3b2703610090d2929d0677f9ae
Hernán Burgos <122620259+burgosha@users.noreply.github.com>
2023-04-19 23:21:58 -0300

    Cracion componente inicial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import RatingComponent from './components/RatingComponent';
 
 function App() {
   return (
     <div className="App">
-
+      <RatingComponent />
     </div>
   );
 }
