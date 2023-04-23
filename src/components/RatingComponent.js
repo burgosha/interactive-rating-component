a01e98e8be3995ea47f0283e587a3d8917a6512f
Hernán Burgos <122620259+burgosha@users.noreply.github.com>
2023-04-23 19:21:27 -0300

    Creacion componente rating

--- src/components/RatingComponent.js
+++ src/components/RatingComponent.js
@@ -1,19 +1,14 @@
 import React from "react";
 import IconStar from "../images/icon-star.svg"
 import "../stylesheets/RatingComponent.css";
+import RatingNumbers from "./RatingNumbers";
 function RatingComponent(props) {
     return(
         <div className="main-component">
             <div className="start-logo"><img src={IconStar} alt="Star Icon"/></div>
             <p className="question">How did we do?</p>
             <p className="description">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
-            <div className="rating-numbers">
-                <div className="number">1</div>
-                <div className="number">2</div>
-                <div className="number">3</div>
-                <div className="number">4</div>
-                <div className="number">5</div>
-            </div>
+            <RatingNumbers />
             <button className="btn-submit">Submit</button>
         </div>
     )
---
+++ src/components/RatingNumbers.js
@@ -0,0 +1,13 @@
+import React from "react";
+function RatingNumbers (props) {
+    return(
+    <div className="rating-numbers">
+        <div className="number">1</div>
+        <div className="number">2</div>
+        <div className="number">3</div>
+        <div className="number">4</div>
+        <div className="number">5</div>
+    </div>
+    );
+}
+export default RatingNumbers;
