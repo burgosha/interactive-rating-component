0bd7bf76ed79f5b3710ffc2812e443961400da54
Hernán Burgos <122620259+burgosha@users.noreply.github.com>
2023-04-24 23:23:35 -0300

    Renderizado condicional

--- src/components/RatingComponent.js
+++ src/components/RatingComponent.js
@@ -4,30 +4,44 @@ import IconStar from "../images/icon-star.svg"
 import "../stylesheets/RatingComponent.css";
 
 function RatingComponent(props) {
+    const [state, setState] = useState(0);
+
     const [rating, setRating] = useState(0);
 
-    return(
-        <div className="main-component">
-            <div className="start-logo"><img src={IconStar} alt="Star Icon"/></div>
-            <p className="question">How did we do?</p>
-            <p className="description">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
-            <div className="rating-numbers">
-                {[...Array(5)].map((number, index) => {
-                    index += 1;
-                    return(
-                        <button
-                            type="button"
-                            key={index}
-                            className={index === rating ? "number-on" : "number"}
-                            onClick={() => setRating(index)}
-                        >
-                            {index}
-                        </button>
-                    )
-                })}
+    const handleClick = submit => {
+        setState(rating);
+    }
+    
+    if(state === 0) {
+        return(
+            <div className="main-component">
+                <div className="start-logo"><img src={IconStar} alt="Star Icon"/></div>
+                <p className="question">How did we do?</p>
+                <p className="description">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
+                <div className="rating-numbers">
+                    {[...Array(5)].map((number, index) => {
+                        index += 1;
+                        return(
+                            <button
+                                type="button"
+                                key={index}
+                                className={index === rating ? "number-on" : "number"}
+                                onClick={() => setRating(index)}
+                            >
+                                {index}
+                            </button>
+                        )
+                    })}
+                </div>
+                <button className="btn-submit" onClick={handleClick}>Submit</button>
             </div>
-            <button className="btn-submit">Submit</button>
-        </div>
-    )
+        )
+    } else {
+        return(
+            <div className="main-component">
+                
+            </div>
+        )
+    }
 }
 export default RatingComponent;
