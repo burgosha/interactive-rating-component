1452f4b474228205af4def7760677da68f2c94ba
Hernán Burgos <122620259+burgosha@users.noreply.github.com>
2023-04-27 19:56:02 -0300

    Attribution

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ function App() {
   return (
     <div className="App">
       <RatingComponent />
+      <div class="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+        Coded by <a href="https://github.com/burgosha" target='_blank'>Burgos Hernán</a>.
+      </div>
     </div>
   );
 }
